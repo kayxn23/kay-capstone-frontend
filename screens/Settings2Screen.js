4c9819d86d1884a30d2c815d94a7ada6db7514b2
Kay <kayxn23@gmail.com>
2019-01-16 21:43:09 -0800

    added boilerplate screens

---
+++ screens/Settings2Screen.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Settings2Screen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Settings2Screen</Text>
+        <Text> Settings2Screen</Text>
+        <Text> Settings2Screen</Text>
+        <Text> Settings2Screen</Text>
+        <Text> Settings2Screen</Text>
+        <Text> Settings2Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default Settings2Screen;
